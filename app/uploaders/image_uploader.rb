4fd99279487e578ac34c7b3b0d58fba9ee73e0c4
大森優太 <>
2020-07-30 20:39:41 +0900

    change

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,6 +44,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
+  def filename
+    original_filename if original_filename
+  end
+
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
