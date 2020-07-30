a472df870a9c51fa13c2dedfae7beb3eb36d41c0
大森優太 <>
2020-07-30 20:04:48 +0900

    fix typo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ unless Rails.env.development? || Rails.env.test?
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV["AWS_ACCESS_ID"],
-      aws_secret_access_key: ENV["AES_SECRET_KEY"],
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"],
       region: 'ap-northeast-1'
     }
     config.fog_provider = 'fog/aws'
