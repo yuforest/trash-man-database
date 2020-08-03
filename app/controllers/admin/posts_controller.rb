e0a61265b66c6f0a89e06b0397693046ac955e4e
大森優太 <>
2020-08-04 01:35:28 +0900

    - headerの検索とりあえずコメントアウト - ページネーション改善

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,5 +1,6 @@
 class Admin::PostsController < Admin::ApplicationController
   def index
     @posts  = Post.search_by_user_id(params[:user_id])
+                  .page(params[:page]).per(5)
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < Admin::ApplicationController
   def index
-    @users  = User.all
+    @users = User.all
+                  .page(params[:page]).per(5)
   end
 end
