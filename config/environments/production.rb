05d12ab36a87fc7da07ccdd745577dc43a823971
大森優太 <>
2020-08-06 22:44:01 +0900

    mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,17 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            'lostforest0@gmail.com',
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
