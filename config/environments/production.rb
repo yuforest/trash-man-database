af9afe0a234260ec31eff5879231d1d2ec1bf260
大森優太 <>
2020-08-06 23:19:22 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
     domain:               'gmail.com',
     user_name:            'info.forest.services@gmail.com',
     password:             pass,
-    authentication:       'plain',
+    authentication:       :login,
     enable_starttls_auto: true
   }
   # Full error reports are disabled and caching is turned on.
