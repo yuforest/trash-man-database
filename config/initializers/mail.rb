d7e6e1f332a8abb7a1128ab3c11a03e8216b3e0c
大森優太 <>
2020-08-07 11:37:33 +0900

    fix error

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.action_mailer.raise_delivery_errors = true
+  ActionMailer::Base.action_mailer.smtp_settings = {
     port: 587,
     address: 'smtp.gmail.com',
     user_name: ENV["SMTP_USERNAME"],
@@ -10,7 +10,7 @@ if Rails.env.production?
     enable_starttls_auto: true
   }
 elsif Rails.env.development?
-  config.action_mailer.delivery_method = :letter_opener
+  ActionMailer::Base.delivery_method = :letter_opener
 else
-  config.action_mailer.delivery_method = :test
+  ActionMailer::Base.delivery_method = :test
 end
