701691b01a302751ca115766587deee39e826ad6
大森優太 <>
2020-08-07 11:38:19 +0900

    fix

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.action_mailer.raise_delivery_errors = true
-  ActionMailer::Base.action_mailer.smtp_settings = {
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = {
     port: 587,
     address: 'smtp.gmail.com',
     user_name: ENV["SMTP_USERNAME"],
